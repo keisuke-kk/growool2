4fcc5812f974b34bdc0ce113047be7d061550bbd
keisuke-kk <candy0220.k@gmail.com>
2020-08-24 14:59:03 +0900

    fix

--- app/frontend/app.vue
+++ app/frontend/app.vue
@@ -9,68 +9,6 @@ export default {
     }
   }
 }
-
-document.addEventListener('DOMContentLoaded',() => {
-  Vue.component('fade-in1', {
-    template: `
-    <div>
-      <transition name="fade">
-        <slot v-if="visible"></slot>
-      </transition>
-    </div>`,
-    data() {
-      return {
-        visible: false
-      };
-    },
-    created() {
-      window.addEventListener("load", this.handleScroll);
-    },
-    destroyed() {
-      window.removeEventListener("load", this.handleScroll);
-    },
-    methods: {
-     handleScroll() {
-       if (!this.visible) {
-         var top = this.$el.getBoundingClientRect().top;
-         this.visible = top < window.innerHeight + 100;
-       }
-     } 
-    }
-  })
-
-  Vue.component('fade-in2', {
-    template: `
-    <div>
-      <transition name="fade">
-        <slot v-if="visible"></slot>
-      </transition>
-    </div>`,
-    data() {
-      return {
-        visible: false
-      };
-    },
-    created() {
-      window.addEventListener("scroll", this.handleScroll);
-    },
-    destroyed() {
-      window.removeEventListener("scroll", this.handleScroll);
-    },
-    methods: {
-     handleScroll() {
-       if (!this.visible) {
-         var top = this.$el.getBoundingClientRect().top;
-         this.visible = top < window.innerHeight + 100;
-       }
-     } 
-    }
-  })
-
-  var app = new Vue({
-    el: '#app'
-  })
-})
 </script>
 
 <style scoped>
